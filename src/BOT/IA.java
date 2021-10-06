7dcecb2756580e9ae4fe8b9b86188e060a16afa2
LEON JR FERREIRA <juniohuy2gamer@gmail.com>
2021-10-06 20:44:19 -0300

    First commit

---
+++ src/BOT/IA.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BOT;
+
+/**
+ *
+ * @author user
+ */
+public class IA {
+  public void teste(){
+      System.out.println("ACESSADO A IA");
+  }
+}
